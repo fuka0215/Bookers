3b18ce479c4544a96f4db121579bbc0e40194512
fuka0215 <162338606+fuka0215@users.noreply.github.com>
2024-04-20 02:44:28 +0000

    [update]3-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/'
-    # redirect先をあとでbooksに変更する
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -19,6 +18,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
